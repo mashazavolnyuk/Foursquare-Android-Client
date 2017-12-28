24d2212ca4813d891684ede6307e59b7164d0706
DESKTOP-0SQTNBI\user <mashazavolnyuk@yandex.ru>
2017-12-28 19:27:34 +0200

    Fixed saving current screen after rotate.

--- app/src/main/java/com/mashazavolnyuk/client/MainActivity.java
+++ app/src/main/java/com/mashazavolnyuk/client/MainActivity.java
@@ -15,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        goToListPlaces();
+        if(getFragmentManager().getBackStackEntryCount() == 0) {
+            goToListPlaces();
+        }
     }
 
     public void goToListPlaces() {
